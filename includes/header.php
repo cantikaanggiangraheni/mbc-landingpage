b6cbe1b9a78fea0bfda792162b4440de2553a927
cantikaanggianggraheni <cantikaanggianggraheni@gmail.com>
2025-07-05 00:34:25 +0700

    Mengembalikan index.php ke api dan update vercel.json

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="logo">MBC Laboratory</h1>
     <nav class="nav-links">
-      <a href="index.php">Home</a>
+      <a href="api/index.php">Home</a>
       <a href="services.php">Divisi</a>
       <a href="contact.php">Kontak</a>
       <a href="developer.php">Developer</a>
